7182c250c39fafb44bac9a87d5f008ad3a2370c9
NicoBellick <nicolas.soumier@gmail.com>
2020-12-17 14:34:49 +0100

    First feature, no responsive yet

---
+++ fiche.js
@@ -0,0 +1,9 @@
+
+const palet = document.getElementById("1")
+let img = document.querySelector(".slide")
+console.log(palet)
+
+palet.addEventListener(onclick, function(e){
+    document.img.style.backgroundImage = "url('bluetooth.jpg')";
+})
+
